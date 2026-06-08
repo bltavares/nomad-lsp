252401d49398c326a7a34357277df0c048851250
Bruno Tavares <connect+github@bltavares.com>
2026-06-08 23:44:31 +0000

    Replace custom hcldec parsing with Nomad upstream jobspec2

--- nomadstructs/diags.go
+++ nomadstructs/diags.go
@@ -2,12 +2,12 @@ package nomadstructs
 
 import (
 	"fmt"
-	"github.com/hashicorp/hcl2/hcl"
-	"github.com/hashicorp/hcl2/hcl/hclsyntax"
-	"github.com/hashicorp/hcl2/hcldec"
-	"github.com/sourcegraph/go-lsp"
 	"os"
-	"reflect"
+	"strings"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/nomad/jobspec2"
+	"github.com/sourcegraph/go-lsp"
 )
 
 func GetDiagnostics(fileName string, originalFile string) []lsp.Diagnostic {
@@ -19,8 +19,17 @@ func GetDiagnostics(fileName string, originalFile string) []lsp.Diagnostic {
 	if _, err := os.Stat(originalFile); os.IsNotExist(err) {
 		originalFile = fileName
 	}
+	file, _ := os.Open(fileName)
+	_, err := jobspec2.Parse(fileName, file)
+	if err == nil {
+		return result
+	}
 
-	hclBody, hclDiags := LoadHCLFile(fileName)
+	hclDiags, ok := err.(hcl.Diagnostics)
+	if !ok {
+		result = extractErrorString(err, result)
+		return result
+	}
 
 	// Need check for docker mounts fix
 	for _, diag := range hclDiags {
@@ -41,69 +50,35 @@ func GetDiagnostics(fileName string, originalFile string) []lsp.Diagnostic {
 		})
 	}
 
-	for _, v := range hclBody.(*hclsyntax.Body).Blocks {
-		for _, p := range v.Body.Blocks {
-			if p.Type == "group" {
-				for _, m := range p.Body.Blocks {
-					if m.Type == "task" {
-						if driverAttr := m.Body.Attributes["driver"]; driverAttr != nil {
-							if reflect.TypeOf(driverAttr.Expr) == reflect.TypeOf(&hclsyntax.TemplateExpr{}) {
-								driverName := driverAttr.Expr.(*hclsyntax.TemplateExpr).Parts[0].(*hclsyntax.LiteralValueExpr).Val.AsString()
-								for i, n := range m.Body.Blocks {
-									if n.Type == "config" {
-										if driverSpec := GetDriverSpec(driverName); driverSpec != nil {
-											body := SanitizeDriverConfig(n.Body, driverName)
-											_, driverDiags := hcldec.Decode(body, driverSpec, &hcl.EvalContext{})
+	return result
+}
 
-											for _, diag := range driverDiags {
-												result = append(result, lsp.Diagnostic{
-													Severity: lsp.DiagnosticSeverity(diag.Severity),
-													Message:  diag.Detail,
-													Range: lsp.Range{
-														Start: lsp.Position{
-															Line:      diag.Subject.Start.Line - 1,
-															Character: diag.Subject.Start.Column - 1,
-														},
-														End: lsp.Position{
-															Line:      diag.Subject.End.Line - 1,
-															Character: diag.Subject.End.Column - 1,
-														},
-													},
-													Source: fmt.Sprintf(" Task Driver(%s)", driverName),
-												})
-											}
-										}
-										m.Body.Blocks = append(m.Body.Blocks[:i], m.Body.Blocks[i+1:]...)
+func extractErrorString(err error, result []lsp.Diagnostic) []lsp.Diagnostic {
+	diagLine := strings.Split(err.Error(), ":")
+	snipRange := strings.TrimSpace(diagLine[1])
+	message := strings.TrimSpace(diagLine[2])
 
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+	var startLine, startColumn, endLine, endColumn int
+	if _, err := fmt.Sscanf(snipRange, "%d,%d-%d,%d", &startLine, &startColumn, &endLine, &endColumn); err != nil {
+		if _, err := fmt.Sscanf(snipRange, "%d,%d-%d", &startLine, &startColumn, &endColumn); err == nil {
+			endLine = startLine
 		}
 	}
 
-	_, nomadDiags := hcldec.Decode(hclBody, JobSpec, &hcl.EvalContext{})
-
-	for _, diag := range nomadDiags {
-		result = append(result, lsp.Diagnostic{
-			Severity: lsp.DiagnosticSeverity(diag.Severity),
-			Message:  diag.Detail,
-			Range: lsp.Range{
-				Start: lsp.Position{
-					Line:      diag.Subject.Start.Line - 1,
-					Character: diag.Subject.Start.Column - 1,
-				},
-				End: lsp.Position{
-					Line:      diag.Subject.End.Line - 1,
-					Character: diag.Subject.End.Column - 1,
-				},
+	result = append(result, lsp.Diagnostic{
+		Severity: lsp.Error,
+		Message:  message,
+		Range: lsp.Range{
+			Start: lsp.Position{
+				Line:      startLine - 1,
+				Character: startColumn - 1,
 			},
-			Source: "Nomad",
-		})
-	}
-
+			End: lsp.Position{
+				Line:      endLine - 1,
+				Character: endColumn - 1,
+			},
+		},
+		Source: "HCL",
+	})
 	return result
 }
